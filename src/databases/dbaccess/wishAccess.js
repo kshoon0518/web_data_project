3cb6408568237794ec0ef441029e2a7731d827e5
joel <whdduf972@gmail.com>
2023-03-21 18:04:49 +0900

    patch : wish API path 수정 및 오류 제거

--- src/databases/dbaccess/wishAccess.js
+++ src/databases/dbaccess/wishAccess.js
@@ -2,28 +2,130 @@ import { prisma } from "./";
 
 const wishAccess = {
   // station_id, user_id, id(wish) 모두를 기준으로 검색할 수 있도록
-  async wishFindMany(findInfo) {
+  async wishStationFindMany(findInfo) {
     const foundWishList = await prisma.wishList.findMany({
-      where: findInfo,
+      where: { station_id: findInfo.station_id },
+      select: {
+        id: true,
+        station: {
+          select: {
+            id: true,
+            station_name: true,
+            station_line: true,
+          },
+        },
+        user: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        createdAt: true,
+        updatedAt: true,
+        deletedAt: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
     });
 
     // 검색한 목록 반환
     return foundWishList;
   },
 
-  // 사용자의 찜 전체 목록 findMany
-  //   async wishFindMany(userId) {
-  //     const foundWishList = await prisma.wish.findMany({
-  //       where: userId,
-  //     });
-  //     // 검색한 목록 반환
-  //     return foundWishList;
-  //   },
+  async wishUserFindMany(findInfo) {
+    const foundWishList = await prisma.wishList.findMany({
+      where: {
+        station_id: findInfo.station_id,
+        user_id: findInfo.user_id,
+      },
+      select: {
+        id: true,
+        station: {
+          select: {
+            id: true,
+            station_name: true,
+            station_line: true,
+          },
+        },
+        user: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        createdAt: true,
+        updatedAt: true,
+        deletedAt: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    // 검색한 목록 반환
+    return foundWishList;
+  },
+
+  async wishUserOnlyFindMany(findInfo) {
+    const foundWishList = await prisma.wishList.findMany({
+      where: {
+        user_id: findInfo.user_id,
+      },
+      select: {
+        id: true,
+        station: {
+          select: {
+            id: true,
+            station_name: true,
+            station_line: true,
+          },
+        },
+        user: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        createdAt: true,
+        updatedAt: true,
+        deletedAt: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    // 검색한 목록 반환
+    return foundWishList;
+  },
 
   // 특정 찜 내역을 검색
   async wishFindUnique(wishInfo) {
     const foundUserWish = await prisma.wishList.findUnique({
       where: wishInfo,
+      select: {
+        id: true,
+        station: {
+          select: {
+            id: true,
+            station_name: true,
+            station_line: true,
+          },
+        },
+        user: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        createdAt: true,
+        updatedAt: true,
+        deletedAt: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
     });
 
     // 검색한 찜 내역 반환
@@ -32,7 +134,12 @@ const wishAccess = {
 
   // 찜 내역 생성
   async wishCreate(wishInfo) {
-    const newWish = await prisma.wishList.create({ data: wishInfo });
+    const newWish = await prisma.wishList.create({
+      data: {
+        user_id: wishInfo.userId,
+        station_id: wishInfo.stationId,
+      },
+    });
 
     // 생성한 찜 내역 반환
     return newWish;
@@ -40,8 +147,10 @@ const wishAccess = {
 
   // id 값 기준으로 찜 내역 삭제
   async wishDelete(deleteInfo) {
-    console.log(deleteInfo);
+    console.log("deleteInfo: ", deleteInfo);
     await prisma.wishList.deleteMany({ where: deleteInfo });
+    console.log("삭제 성공?");
+
     return;
   },
 };
