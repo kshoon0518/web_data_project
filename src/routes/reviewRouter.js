23631da16202c48353ce07a2895d846ef8362ba5
seungeun Lee <dltmddms0819@gmail.com>
2023-03-23 11:01:18 +0900

    stationAddress 데이터 삭제

--- src/routes/reviewRouter.js
+++ src/routes/reviewRouter.js
@@ -14,12 +14,7 @@ reviewRouter.post("/review/:station_id", isUser, async (req, res, next) => {
       user_id,
       body,
     });
-    const isSuccess =
-      newReview != null
-        ? "리뷰 작성에 성공하였습니다."
-        : "리뷰 작성에 실패하였습니다.";
-    console.log(isSuccess);
-    res.status(201).json({ message: "newReview" });
+    res.status(201).json({ message: "리뷰작성에 성공했습니다." });
   } catch (err) {
     next(err);
   }
@@ -66,7 +61,6 @@ reviewRouter.patch("/review/:review_id", isUser, async (req, res, next) => {
       console.log("updatedreview:", updatedreview);
       res.status(200).json({
         message: "리뷰가 수정되었습니다.",
-        updatedreview: updatedreview,
       });
     } else {
       res.status(200).json({
